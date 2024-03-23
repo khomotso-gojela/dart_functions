3367443a13d292ded3d838b7890c7b2ce8bdb3c9
khomotso gojela <motsoak49@gmail.com>
2024-03-23 07:37:18 +0200

    first commit

---
+++ main.dart
@@ -0,0 +1,25 @@
+void main() {}
+
+int addTwo(int num1, int num2) {
+  return num1 + num2;
+}
+
+int subtractTwo(int num1, int num2) {
+  return num1 - num2;
+}
+
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+
+double divideTwo(int num1, int num2) {
+  return num1 / num2;
+}
+
+int stringLength(String text) {
+  return text.length;
+}
+
+String getFirstElement(List list) {
+  return list[0];
+}
